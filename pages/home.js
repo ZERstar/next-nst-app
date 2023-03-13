497c03d3b9f170a6aa56d122105d161aca60c092
Sanjeeban2411 <sanju.sanjeeban.sp@gmail.com>
2023-03-14 00:30:22 +0530

    image upload and display

---
+++ pages/home.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import Head from "next/head";
+
+export default function about() {
+  const [file1, setFile1] = useState(null);
+  const [file2, setFile2] = useState(null);
+  function loadImg1(e) {
+    console.log("111", e.target.files);
+    setFile1(URL.createObjectURL(e.target.files[0]));
+  }
+  function loadImg2(e) {
+    console.log("222", e.target.files);
+    setFile2(URL.createObjectURL(e.target.files[0]));
+  }
+  return (
+    <div>
+      <div className="nav h-[10vh] bg-slate-400 flex flex-row justify-between items-center px-16">
+        <div className="text-[#141E30] text-3xl  ">
+          <i>D</i>iffuse
+          <i>
+            <b>D</b>
+          </i>
+          rip
+        </div>
+        <div>
+          <button className="h-10 px-4 rounded-full bg-slate-700/20 text-[#141E30] border-[#141E30] border-2 font-bold">
+            Connect Wallet
+          </button>
+        </div>
+      </div>
+      <div className="bg-gradient-to-t from-[#141E30] to-[#243B55]">
+        <div className="h-[90vh]">
+          <div className="flex flex-row justify-around pt-20">
+            <div className={`border-2 border-white`}>
+              <img src={file1} alt="abc1" className="w-60 h-60" />
+            </div>
+            <div className={`border-2 border-white `}>
+              <img src={file2} alt="abc2" className="w-60 h-60" />
+            </div>
+          </div>
+          <div className="flex flex-row justify-around pt-20 px-40">
+            {/* <button className="bg-white px-6 py-2">UPLOAD</button> */}
+            <div>
+              <label htmlFor="fileSelect1" className="bg-white px-6 py-2">
+                <input
+                  id="fileSelect1"
+                  type="file"
+                  // multiple
+                  className="hidden w-64 h-60"
+                  style={{ width: "100px", height: "100px" }}
+                  onChange={loadImg1}
+                />
+                UPLOAD1
+              </label>
+              {file1 && (
+                <div>
+                  <button className="bg-white px-6 py-2" onClick={()=>setFile1(null)}>
+                    DELETE1
+                  </button>
+                </div>
+              )} 
+            </div>
+            <button className="bg-white px-6 py-2">GEN</button>
+            <div>
+              <label htmlFor="fileSelect2" className="bg-white px-6 py-2">
+                <input
+                  id="fileSelect2"
+                  type="file"
+                  // multiple
+                  className="hidden w-64 h-60"
+                  style={{ width: "100px", height: "100px" }}
+                  onChange={loadImg2}
+                />
+                UPLOAD2
+              </label>
+              {file2 && (
+                <div>
+                  <button className="bg-white px-6 py-2" onClick={()=>setFile2(null)}>
+                    DELETE2
+                  </button>
+                </div>
+              )} 
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Router from 'next/router'
 export default function Index() {
   return (
     <>
@@ -8,6 +9,29 @@ export default function Index() {
           <meta name ="viewport" content="width=device-width,
           initial-scale=1" />
         </Head>
+        <div>
+          <div className="nav h-[10vh] bg-slate-400 flex flex-row justify-between items-center px-16">
+            <div className="text-[#141E30] text-3xl  ">
+              <i>D</i>iffuse<i><b>D</b></i>rip
+            </div>
+            <div>
+              <button className="h-10 px-4 rounded-full bg-slate-700/20 text-[#141E30] border-[#141E30] border-2 font-bold">
+                Connect Wallet
+              </button>
+            </div>
+          </div>
+          <div className="bg-gradient-to-t from-[#141E30] to-[#243B55] h-[90vh]">
+            acd
+            <button 
+            onClick={()=>{
+              Router.push('/home')
+            }}
+            className="bg-white"
+            >
+              TRY ---&gt;
+            </button>
+          </div>
+        </div>
     </>
   )
 }
