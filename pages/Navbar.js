668fa3c3b0f2282caa60e58ec6e8c068961aa95f
ZERstar <dumbretejas1@gmail.com>
2023-03-16 23:22:07 +0530

    Navbar file

---
+++ pages/Navbar.js
@@ -0,0 +1,26 @@
+
+export default function Navbar() {
+  return (
+    <>
+    <header>
+    <nav>
+    <div className="nav h-[10vh] bg-slate-400 flex flex-row justify-between items-center px-16">
+        <div className="text-[#141E30] text-3xl  ">
+          <i>D</i>iffuse
+          <i>
+            <b>D</b>
+          </i>
+          rip
+        </div>
+        <div>
+          <button className="h-10 px-4 rounded-full bg-slate-700/20 text-[#141E30] border-[#141E30] border-2 font-bold">
+            Connect Wallet
+          </button>
+        </div>
+      </div>
+      </nav>
+      </header>
+      </>
+  )
+}
+
--- pages/home.js
+++ pages/home.js
@@ -14,20 +14,6 @@ export default function about() {
   }
   return (
     <div>
-      <div className="nav h-[10vh] bg-slate-400 flex flex-row justify-between items-center px-16">
-        <div className="text-[#141E30] text-3xl  ">
-          <i>D</i>iffuse
-          <i>
-            <b>D</b>
-          </i>
-          rip
-        </div>
-        <div>
-          <button className="h-10 px-4 rounded-full bg-slate-700/20 text-[#141E30] border-[#141E30] border-2 font-bold">
-            Connect Wallet
-          </button>
-        </div>
-      </div>
       <div className="bg-gradient-to-t from-[#141E30] to-[#243B55]">
         <div className="h-[90vh]">
           <div className="flex flex-row justify-around pt-20">
